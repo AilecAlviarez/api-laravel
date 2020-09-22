5e822e595eec4a409bfb4c6f0e6c0c5f47638345
maria de los angeles <mariaramireeez2002@gmail.com>
2020-09-22 18:48:54 -0400

    cambiaron los campos user para coincidir con el login

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
     protected $primaryKey='user_id';
     protected $fillable = [
-        'user_name', 'user_email', 'user_password','role','saldo'
+        'name', 'email', 'password','role','saldo'
     ];
 
     /**
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'user_name' => $this->faker->name,
-            'user_email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'role'=>$role=$this->faker->randomElement([User::ADMIN,User::NOADMIN]),
-            'user_password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
             'saldo'=>($role==User::NOADMIN)?$this->faker->randomFloat(1,10,200):null,
             'remember_token' => Str::random(10)
 
