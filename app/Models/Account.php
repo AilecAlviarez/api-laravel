95485242355c834cd3363bf261fdca6dad0a6627
maria de los angeles <mariaramireeez2002@gmail.com>
2020-09-25 12:34:06 -0400

    definiendo en account la relacion con un usuario

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -10,8 +10,11 @@ class Account extends Model
     use HasFactory;
     protected $primaryKey='account_id';
     protected $table='accounts';
-    protected $fillable=['bank_id','account_number'];
+    protected $fillable=['bank_id','account_number','user_id'];
     public function bank(){
         return $this->hasOne(Bank::class,'bank_id','bank_id');
+    }
+    public function Buyer(){
+        return $this->hasOne(Buyer::class,'user_id','user_id');
     }
 }
